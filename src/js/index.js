bb184a105ab11874b7c9e2f585c10e968832e1ab
Masyanyaz <47759559+Masyanyaz@users.noreply.github.com>
2020-03-24 14:24:27 +0300

    'asdasd'

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-console.log('asdasd');
+window.onload = () => {
+  document.querySelector('.go-to-top').addEventListener('click', e => {
+    e.preventDefault();
+
+  })
+}
+$(document).ready(function () {
+  $('.header__burger').on('click', function () {
+    const headerMobile = document.querySelector('.header__mobile-menu');
+    if (headerMobile.classList.contains('mb-active')) {
+      headerMobile.classList.remove('mb-active');
+    } else {
+      headerMobile.classList.add('mb-active');
+    }
+  });
+});
